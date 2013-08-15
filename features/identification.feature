0b0582a377524d60226d702d5d829a7bba74314d
jmarzin <jacques.marzin@free.fr>
2013-08-15 12:02:39 +0200

    modified:   app/views/devise/sessions/new.html.haml 	modified:   app/views/layouts/_header.html.haml 	modified:   config/locales/devise.en.yml 	modified:   features/identification.feature 	modified:   features/step_definitions/identification_steps.rb

--- features/identification.feature
+++ features/identification.feature
@@ -24,8 +24,24 @@ Fonctionnalité: Identification des utilisateurs
     Quand je suis sur la page /
     Quand je suis le lien [Connexion]
     Quand je suis le lien Créer un compte
-    Quand je saisis l'adresse jacques.marzin@free.fr et le mot de passe 51julie2
+    Quand je saisis l'adresse jacques.marzin@free.fr et 2 fois le mot de passe 51julie2
     Quand je clique sur le bouton Créer le compte
     Alors je me retrouve sur la page /
     Et je vois le texte "jacques.marzin@free.fr"
     Et je vois le texte "Bienvenue ! Votre compte a été créé."
+
+  Scénario: Une fois mon compte créé, je peux me connecter
+    Etant donné que j'ai le compte jacques.marzin@free.fr avec le mot de passe 51julie2 dans le système
+    Quand je suis sur la page /
+    Quand je suis le lien [Connexion]
+    Quand je saisis mon adresse jacques.marzin@free.fr et mon mot de passe 51julie2
+    Quand je clique sur le bouton Se connecter
+    Alors je me retrouve sur la page /
+    Et je vois le texte "jacques.marzin@free.fr"
+    Et je vois le texte "Vous êtes maintenant connecté(e)"
+
+  Scénario: Quand je suis connecté, je peux me déconnecter
+    Etant donné que je suis connecté au système
+    Quand je suis le lien [Déconnexion]
+    Alors je vois le texte "Vous n'êtes pas connecté(e)"
+    Et je vois le texte "Vous êtes maintenant déconnecté(e)"
--- features/step_definitions/identification_steps.rb
+++ features/step_definitions/identification_steps.rb
@@ -12,17 +12,41 @@ Quand(/^je suis le lien (.+)$/) do |lien|
   click_link(lien)
 end
 
-Quand(/^je saisis l'adresse (.+) et le mot de passe (.+)$/) do |adresse,mdp|
-  fill_in 'user_email', :with => adresse
+Quand(/^je saisis (?:l'|mon )adresse (.+) et (|2 fois )(?:le|mon) mot de passe (.+)$/) do |compte,creation,mdp|
+  fill_in 'user_email', :with => compte
   fill_in 'user_password', :with => mdp
-  fill_in 'user_password_confirmation', :with => mdp
+  if creation =~ /2 fois/ then fill_in 'user_password_confirmation', :with => mdp end
 end
 
-Quand(/^je clique sur le bouton Créer le compte$/) do
-  click_button('Créer le compte')
+Quand(/^je clique sur le bouton (.+)$/) do |bouton|
+  click_button(bouton)
 end
 
 Alors /^(?:|je )me retrouve sur la page (.+)$/ do |page_name|
   current_path.should eq page_name
 end
 
+Etantdonné(/^que j'ai le compte (.+) avec le mot de passe (.+) dans le système$/) do |compte, mdp|
+  User.new(:email => compte, :password => mdp, :password_confirmation => mdp).save!
+end
+
+Etantdonné(/^que j'ai un compte dans le système$/) do
+  compte = 'jacques.marzin@free.fr'
+  mdp = '51julie2'
+  User.new(:email => compte, :password => mdp, :password_confirmation => mdp).save!
+end
+
+Quand(/^je saisis mon adresse et mon mot de passe$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Etantdonné(/^que je suis connecté au système$/) do
+  compte = 'jacques.marzin@free.fr'
+  mdp = '51julie2'
+  User.new(:email => compte, :password => mdp, :password_confirmation => mdp).save!
+
+  visit '/my/users/sign_in'
+  fill_in "user_email", :with => compte
+  fill_in "user_password", :with => mdp
+  click_button "Se connecter"
+end
